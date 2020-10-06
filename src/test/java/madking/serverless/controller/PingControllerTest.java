5d4f899763fdf1bc092b9b4f38847194c5c1e9e9
Mad King <madking.direct@gmail.com>
2020-10-06 09:16:27 +0100

    Adding unit testing

---
+++ src/test/java/madking/serverless/controller/PingControllerTest.java
@@ -0,0 +1,23 @@
+package madking.serverless.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("local")
+class PingControllerTest {
+
+    @Autowired
+    private PingController pingController;
+
+    @Test
+    void processMimeMessage() throws Exception {
+
+
+    }
+
+}
